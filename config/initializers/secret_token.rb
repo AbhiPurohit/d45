1fdb9d3a16bc0283e413269a224a920fabe28540
Abhishek Purohit <careerabhi@yahoo.com>
2012-11-12 16:13:29 +1100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+D45::Application.config.secret_token = 'f093e90fce89800eb7f05282ea6603656bf21767065e2b82fa55980d13dafb06a8cfbf029799fda6cd0144386a1d28feb89f9e1e06aff4f66e915131bcd742e9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+D45::Application.config.session_store :cookie_store, key: '_d45_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# D45::Application.config.session_store :active_record_store
